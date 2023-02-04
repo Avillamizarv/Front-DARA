e3ea6d9097bf9307832d2afb5e2e3c9c8cb86d9a
Adriana Villamizar <adriana2190033@correo.uis.edu.co>
2023-02-04 10:31:12 -0500

    Feat: main page component was created

--- dara/src/app/app-routing.module.ts
+++ dara/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dara',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dara',
+    component: MainPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- dara/src/app/app.module.ts
+++ dara/src/app/app.module.ts
@@ -4,19 +4,21 @@ import { NgModule } from '@angular/core';
 import { CoreModule, FlexLayoutModule } from '@angular/flex-layout';
 import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainPageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FlexLayoutModule,
     HttpClientModule,
     MaterialModule,
+    BrowserAnimationsModule,
     CoreModule,
     MatDialogModule,
     CommonModule,
