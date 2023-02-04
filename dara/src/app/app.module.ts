96425952aadf8fc88a8770887f9f3ec21666991a
Adriana Villamizar <adriana2190033@correo.uis.edu.co>
2023-02-04 10:03:07 -0500

    Fix: add angular material

--- dara/src/app/app.module.ts
+++ dara/src/app/app.module.ts
@@ -1,18 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { CoreModule, FlexLayoutModule } from '@angular/flex-layout';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    MaterialModule,
+    CoreModule,
+    MatDialogModule,
+    CommonModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: MAT_DIALOG_DATA, useValue: {} },
+    { provide: MatDialogRef, useValue: {} },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ dara/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+
+  urlBackDara: 'https://lovpets.herokuapp.com/lovpets/api',
+};
